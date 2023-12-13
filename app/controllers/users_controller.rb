9f10d25947e7906af258ecbbd3e0150778cacbbc
erikosampaio <erikoa.93@gmail.com>
2023-12-13 01:40:24 -0300

    corrijo total por status e ordeno index principal.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i[ edit update destroy ]
 
   def index
-    @users = User.all.order(:name).order(:confirmed)
+    @users = User.all.order(:confirmed).order(:answered).order(:name)
   end
 
   def index_status
